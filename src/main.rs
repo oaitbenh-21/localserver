c680d9594d505289cb6c9769d03929426032f87f
oaitbenh-21 <0xmrrandom@gmail.com>
2026-02-02 22:26:58 +0100

    just for know what to do

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let http_request_head: String = String::from("POST /api/auth HTTP/1.1"); // use regex or tokenizition to check
+    let parts: Vec<&str> = http_request_head.split(' ').collect();
+    println!("{}", parts[0]); // check length if i use tokenizition and handle each part to get valid method path and protocol/version
 }
-
 
 // parsing :
 // 	method path http_version
